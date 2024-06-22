ce64603a45261d1c08c03d18d3bfb4c5336bab89
Omer Ak <OmerAk@gmail.com>
2024-06-22 22:27:38 +0300

    Update js file

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 let ourSkills = document.querySelector(".ourSkills");
 let progress = document.querySelectorAll(".skill .progress span");
 window.onscroll = function () {
-    if (window.scrollY >= ourSkills.offsetTop) {
+    if (window.scrollY >= ourSkills.offsetTop -100) {
         progress.forEach((span) => {
             span.style.width = span.dataset.width;
         });
