ce70cc5db7578c9762c7354ce435d99da98a3283
Omer Ak <OmerAk@gmail.com>
2024-06-24 22:27:22 +0300

    Update Date

--- js/main.js
+++ js/main.js
@@ -51,4 +51,4 @@ function startCountStats(el) {
 
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
-document.querySelector(".currentYear").innerHTML = currentYear;
+document.querySelector("span.currentYear").innerHTML = currentYear;
